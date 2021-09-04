9ed07e74428b19872fcf8ce8b6d310066ca3e401
Artyom Polyanskiy (Nicothin) <35273601+nicoth-in@users.noreply.github.com>
2021-09-05 01:37:47 +0300

    Backend update: Added options management

---
+++ server/api.py
@@ -0,0 +1,41 @@
+
+import aiohttp
+import asyncio
+import json
+
+async def api_post_data(file_name: str, file_path: str, content_type: str, report: dict):
+    #return ["ok"]
+
+    print(report)
+
+    #auth = base64.encodebytes(b"Obigail:ObigailH4u")
+    auth = 'T2JpZ2FpbDpPYmlnYWlsSDR1'
+
+    headers = {
+        'Content-Type': 'multipart/form-data',
+        'Authorization': f'Basic {auth}',
+    }
+
+    endpoint = 'http://elib-hackathon.psb.netintel.ru/elib/api/service/documents'
+
+    async with aiohttp.ClientSession() as session:
+
+        data = aiohttp.FormData()
+        data.add_field('attachments',
+                       open(file_path, 'rb').read(),
+                       filename=file_path
+                       )
+        data.add_field('createRequest', json.dumps(report))
+
+        # data = {
+        #     'attachments': open(file_path, 'rb'),
+        #     'createRequest': json.dumps(report)
+        # }
+
+        # data = {'attachments': (file_path, open(file_path, 'rb')),
+        #         'createRequest': json.dumps(report)}
+
+        #print(data)
+
+        async with session.post(endpoint, headers=headers, data=data) as resp:
+            print(await resp.json())
--- server/main.py
+++ server/main.py
@@ -1,10 +1,7 @@
+import asyncio
 import base64
 import json
 import re
-
-
-import aiohttp
-import asyncio
 
 
 from PIL import Image
@@ -20,122 +17,17 @@ import pandas as pd
 import textract
 import os
 import csv
-from fastapi import FastAPI, Header
+from fastapi import FastAPI, Header, WebSocket, Request
 import requests
 import base64
 from requests_toolbelt.multipart.encoder import MultipartEncoder
 import mimetypes
 
+from options import file_types
+from api import api_post_data
 
 #auth = 'T2JpZ2FpbDpPYmlnYWlsSDR1'
 
-
-
-
-
-def detect_russian_word(imagePNG):
-    text = pytesseract.image_to_string(Image.open(imagePNG), lang='rus')
-    print(text)
-
-
-file_types = [
-    {
-        "keys": ['Устав', 'Уставный капитал', 'Органы управления', 'Резервный фонд', 'Бюллетени'],
-        "path": ['Юридическое досье', 'Учредительные и иные внутренние документы (положения)'],
-        "code": '33a37ce4-c6a9-4dad-8424-707abd47c125',
-        "name": "Устав_действующий"
-    },
-    {
-        "keys": ['Положение о совете директоров','Председатель совета директоров', 'Письменное мнение', 'Опросный лист'],
-        "path": ['Юридическое досье', 'Учредительные и иные внутренние документы (положения)'],
-        "code": '555ced1c-c169-4d61-9a82-348801494581',
-        "name": "Положение о СД"
-    },
-    {
-        "keys": ['Бухгалтерский баланс','0710001', 'Актив', 'Пассив'],
-        "path": ['Финансовое досье', '$year', '$quart', 'Бухгалтерская отчетность'],
-        "code": '4f501f4a-c665-4cc8-9715-6ed26e7819f2',
-        "name": "Бухгалтерская отчетность_форма 1"
-    },
-    {
-        "keys": ['Отчет о финансовых результатах','0710002','Чистая прибыль','Налог на прибыль'],
-        "path": ['Финансовое досье', '$year', '$quart', 'Бухгалтерская отчетность'],
-        "code": 'cabd193c-f9a9-4a9c-a4ae-80f0347adf40',
-        "name": "Бухгалтерская отчетность_форма 2"
-    },
-    {
-        "keys": ['Бухгалтерский баланс','0710001', 'Актив', 'Пассив',],
-        "path": ['Финансовое досье', '$year', '$quart', 'Бухгалтерская отчетность'],
-        "code": '2e321818-4571-43ae-9e08-2ade54b83e14',
-        "name": "Бухгалтерская отчетность_форма 1 _промежуточная"
-    },
-    {
-        "keys": ['Отчет о финансовых результатах', '0710002', 'Чистая прибыль', 'Налог на прибыль'],
-        "path": ['Финансовое досье', '$year', '$quart', 'Бухгалтерская отчетность'],
-        "code": '3b4f4647-f755-4100-bd63-059627107919',
-        "name": "Бухгалтерская отчетность_форма 2 _промежуточная"
-    },
-    {
-        "keys": ['Аудиторское заключение', 'Сведения об аудируемом лице', 'Сведения об аудиторе', 'Основание для выражения мнения', 'Ответственность аудитора'],
-        "path": ['Финансовое досье', '$year', '$quart', 'Бухгалтерская отчетность'],
-        "code": '16f35ccc-b90f-4731-8178-11f3e0e3ca20',
-        "name": 'Аудиторское заключение'
-    },
-    {
-        "keys": ['Презентация компании', 'Обзор рынка', 'Обзор компании', 'История компании'],
-        "path": ['Описание бизнеса'],
-        "code": 'a397c2cf-c5ad-4560-bc65-db4f79840f82',
-        "name": "Описание_деятельности_ГК"
-    },
-    {
-        "keys": ['Протокол', 'ИТОГИ ГОЛОСОВАНИЯ', 'решение', 'Принято'],
-        "path": ['Юридическое досье', 'Документы, подтверждающие полномочия на совершение сделки'],
-        "code": '3af37c7f-d8b1-46de-98cc-683b0ffb3513',
-        "name": "Решение_назначение ЕИО"
-    }
-]
-
-# search_list_b_o_f_1 = ['бухгалтерский','баланс', 'форма', 'по', 'окуд', '0710001' ,'актив','пассив']
-# search_list_b_o_f_2 = ['отчет о финансовых результатах','дата', 'форма', 'по', 'окуд', '0710002' ,'чистая прибыль','налог на прибыль']
-# ustav = ['устав','уставный капитал','органы управления','резервный фонд','бюллетени']
-# polozhenie_o_sd= ['положение о совете директоров','председатель совета директоров','письменное мнение','опросный лист','уведомление о проведении cовета директоров']
-# search_list_b_o_f_1_p = ['бухгалтерский','баланс', 'форма', 'по', 'окуд', '0710001' ,'актив','пассив']
-# search_list_b_o_f_2_p = ['отчет о финансовых результатах','дата', 'форма', 'по', 'окуд', '0710002' ,'чистая прибыль','налог на прибыль']
-# auditor_zak = ['аудиторское заключение','сведения об аудируемом лице','сведения об аудиторе','основание для выражения мнения','ответственность аудитора']
-# deyatelinosti_gk = ['презентация компании', 'история компании', 'обзор рынка', 'обзор компании']
-# razreshenie_naznachenie = ['протокол совета директоров','дата составления протокола','избрание/назначение генерального директора','итоги голосования','принятое решение']
-# nome_code_list = ['33a37ce4-c6a9-4dad-8424-707abd47c125','555ced1c-c169-4d61-9a82-348801494581','4f501f4a-c665-4cc8-9715-6ed26e7819f2','cabd193c-f9a9-4a9c-a4ae-80f0347adf40','2e321818-4571-43ae-9e08-2ade54b83e14','3b4f4647-f755-4100-bd63-059627107919','16f35ccc-b90f-4731-8178-11f3e0e3ca20','a397c2cf-c5ad-4560-bc65-db4f79840f82','3af37c7f-d8b1-46de-98cc-683b0ffb3513']
-
-
-async def api_post_data(file_name: str, file_path: str, content_type: str, report: dict):
-    return ["ok"]
-
-    print(report)
-
-    #auth = base64.encodebytes(b"Obigail:ObigailH4u")
-    auth = 'T2JpZ2FpbDpPYmlnYWlsSDR1'
-
-    headers = {
-        'accept': '*/*',
-        'Content-Type': 'multipart/form-data',
-        'Authorization': f'Basic {auth}',
-    }
-
-    endpoint = 'http://elib-hackathon.psb.netintel.ru/elib/api/service/documents'
-
-    async with aiohttp.ClientSession() as session:
-
-        data = aiohttp.FormData()
-        data.add_field('attachments',
-                       open(file_path, 'rb'),
-                       filename=file_path
-                       )
-        data.add_field('createRequest', json.dumps(report))
-
-        print(data)
-
-        async with session.get(endpoint, headers=headers, data=data) as resp:
-            return await resp.json()
 
 
 def pdf2txt(filePath):
@@ -176,29 +68,19 @@ def csv2txt(filePath):
 
 app = FastAPI()
 
-
 ALLOWED_TYPES = ["application/pdf", "application/vnd.ms-excel", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"]
 ROOT_PATH = "/home/ivan/Загрузки/Промсвязьбанк Датасет/Тестовый dataset/"
 
 
-def verify_text(txt, search_list):
-    match = True
-
-    for key in search_list["keys"]:
-        match &= key.lower() in txt
-
-    return match
-
-
 class FileEntry:
-
     full_path: str
     parts: [str]
     guessed_type: str
     name: str
+    text: str
 
 
-def get_all_files(directory: str):
+def get_all_files(directory: str, root_dir: str):
 
     output = []
     for path, subdirs, files in os.walk(directory):
@@ -213,7 +95,7 @@ def get_all_files(directory: str):
                 file.full_path = os.path.join(path, name)
                 file.guessed_type = guessed_type
                 file.name = name
-                file.parts = path.split(directory)[1].split('/')
+                file.parts = path.split(root_dir)[1].split('/')
 
                 for i, part in enumerate(file.parts):
                     if part == "":
@@ -226,70 +108,131 @@ def get_all_files(directory: str):
     return output
 
 
-def verify_file(path: str, ftype: str):
+class ModularCategorizer:
 
-    txt = ""
+    input_path: str
+    root_path: str
+    not_verified_dir: str
+    files: [FileEntry]
+    inn: str
 
-    if ftype == "application/pdf":
-        txt = pdf2txt(path)
+    def analyse_input_path(self):
 
-    elif ftype == "application/vnd.ms-excel" or ftype == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
-        txt = xls2txt(path)
+        # List of directories requested
+        input_paths = self.input_path.split("/")
 
-    txt = txt.lower()
-    print(txt)
+        self.root_path = ROOT_PATH
 
-    #r"([0-9]{1,2})[ ]+(января|февраля|марта|апреля|июня|июля|августа|сентября|октября|ноября|декабря)[ ]+([0-9]{4})"
+        for i, inpath in enumerate(input_paths):
 
-    for search_list in file_types:
-        verified = verify_text(txt, search_list)
-        if verified:
-            return search_list
+            self.root_path = os.path.join(self.root_path, inpath)
 
-    return None
+            # Search for INN in folder name
+            result_inn = re.findall(r"[0-9]{10}", inpath, re.MULTILINE)
 
+            if len(result_inn) > 0:
 
-async def company_verification(inn: str, root_path: str, folder: [str]):
-
-    not_verified_dir = os.path.join(root_path, "Не верифицированные документы")
-
-    output = []
-
-    working_dir = root_path
-
-    for f in folder:
-        working_dir = os.path.join(working_dir, f)
-
-    # Get all files and sub files
-    files = get_all_files(working_dir)
-
-    for file in files:
-
-        ver_result = verify_file(file.full_path, file.guessed_type)
-        if ver_result is None:
-            print("Unrecognized! Moving..")
-            os.rename(file.full_path, os.path.join(not_verified_dir, file.name))
-
-        if ver_result is not None:
-
-            print("Expected", ver_result["path"])
-            print("Got", file.parts)
-
-            print("File", file.name)
-
-            req = {
-                "documentNomenclatureId": ver_result["code"],
-                "inn": inn,
-                "unrecognised": False,
-            }
-
-            resp = await api_post_data(file.name, file.full_path, file.guessed_type, req)
-            print(resp)
+                self.inn = result_inn[0]
+                yield self.company_verification(input_paths[i + 1:])
 
 
+    def company_verification(self, folder: [str]):
+
+        self.not_verified_dir = os.path.join(self.root_path, "Не верифицированные документы")
+
+        working_dir = self.root_path
+
+        for f in folder:
+            working_dir = os.path.join(working_dir, f)
+
+        # Get all files and sub files
+        self.files = get_all_files(working_dir, self.root_path)
+
+        return
+
+
+    async def verificate_one(self, file):
+
+        if file.guessed_type == "application/pdf":
+            txt = pdf2txt(file.full_path)
+
+        elif file.guessed_type == "application/vnd.ms-excel" or file.guessed_type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+            txt = xls2txt(file.full_path)
+        else:
+            return
+
+        txt = txt.lower()
+
+        file.text = txt
+
+        return await self.verify_file(self.inn, file)
+
+    async def verify_file(self, inn, file: FileEntry):
+
+        for category in file_types:
+
+            verified = await self.verify_category(category, file)
+
+            if verified:
+
+                await self.report_to_api(inn, file, category)
+
+                return
+
+        print("Unrecognized! Moving..")
+        os.rename(file.full_path, os.path.join(self.not_verified_dir, file.name))
+
+        await self.report_fail_to_api(inn, file)
+
+        return
+
+
+    async def verify_category(self, category, file) -> bool:
+        matching = await self.require_keys(file, category)
+
+        print("Expected", category["path"])
+        print("Got", file.parts)
+        print("File", file.name)
+
+        data_re = r"([0-9]{1,2})[ ]+(января|февраля|марта|апреля|июня|июля|августа|сентября|октября|ноября|декабря)[ ]+([0-9]{4})"
+        result_data = re.findall(data_re, file.text, re.MULTILINE)
+
+        if len(result_data) > 0:
+            print(result_data[0])
+
+        return matching
+
+    async def require_keys(self, file: FileEntry, category) -> bool:
+        match = True
+
+        for key in category["keys"]:
+            match &= key.lower() in file.text
+
+        return match
+
+
+    async def report_to_api(self, inn, file, category):
+        req = {
+            "documentNomenclatureId": category["code"],
+            "inn": inn,
+            "unrecognised": False,
+        }
+
+        resp = await api_post_data(file.name, file.full_path, file.guessed_type, req)
+
+    async def report_fail_to_api(self, inn, file):
+        req = {
+            "unrecognised": True,
+        }
+
+        resp = await api_post_data(file.name, file.full_path, file.guessed_type, req)
+
+
+    async def send_msg(self, msg):
+        await self.socket.send_text(json.dumps(msg))
 
 @app.get("/files")
-async def get_goods_array(name: str):
+async def get_fs_in_folder(name: str):
 
     fpath = os.path.join(ROOT_PATH, name)
 
@@ -302,24 +245,61 @@ async def get_goods_array(name: str):
     }
 
 
+@app.websocket("/verify")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    path = await websocket.receive_text()
+
+    categorizer = ModularCategorizer()
+    categorizer.input_path = path
+
+    for t in categorizer.analyse_input_path():
+
+        await websocket.send_json({
+            "type": "total",
+            "msg": len(categorizer.files)
+        })
+        print(len(categorizer.files))
+
+        for i, file in enumerate(categorizer.files):
+
+            tasks = []
+            ev = asyncio.Event()
+
+            async def message(r):
+                await websocket.send_json({
+                    "type": "current",
+                    "msg": i + 1
+                })
+                r.set()
+
+            async def runner(f, r):
+                await r.wait()
+                await categorizer.verificate_one(f)
+
+            tasks.append(asyncio.ensure_future(message(ev)))
+            tasks.append(asyncio.ensure_future(runner(file, ev)))
+
+            await asyncio.gather(*tasks)
+
+            print(i + 1)
+
+    return
+
+@app.get("/criteria")
+async def get_criteria():
+
+    global file_types
+
+    return file_types
 
 
-@app.get("/verify")
-async def get_goods_array(path: str):
+@app.post("/criteria")
+async def update_criteria(request: Request):
 
-    # List of directories requested
-    input_paths = path.split("/")
-    root_path = ROOT_PATH
+    global file_types
+    file_types = await request.json()
 
-    for i, inpath in enumerate(input_paths):
-        root_path = os.path.join(root_path, inpath)
-
-        # Search for INN in folder name
-        result_inn = re.findall(r"[0-9]{10}", inpath, re.MULTILINE)
-
-        if len(result_inn) > 0:
-            inn = result_inn[0]
-            # Call for each INN it's own verify
-            await company_verification(inn, root_path, input_paths[i+1:])
-
-    return {}
+    return {
+        "status": "ok"
+    }
